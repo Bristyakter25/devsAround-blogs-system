848ba435726d50add4136cd936e108fcb78fbd81
Bristyakter25 <sabihaakterbristy@gmail.com>
2025-08-28 01:46:39 +0600

    set up completed both server and client side

---
+++ blog-site-client/src/components/Home/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h2 className='text-red-300'>this for checking</h2>
+            <button className="btn btn-success">Success</button>
+        </div>
+    );
+};
+
+export default HomePage;
