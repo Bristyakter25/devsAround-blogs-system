7ef151f888fbebf07171a94c9b2e0042d1284d63
Bristyakter25 <sabihaakterbristy@gmail.com>
2025-08-29 23:51:24 +0600

    added more blog post for another section

--- blog-site-client/src/Pages/Blogs/DisplayCreatedBlogs.jsx
+++ blog-site-client/src/Pages/Blogs/DisplayCreatedBlogs.jsx
@@ -12,35 +12,37 @@ const  DisplayCreatedBlogs= () => {
   }, []);
 
   return (
-    <div className="max-w-5xl mx-auto p-6 space-y-6">
-      <h1 className="text-3xl font-bold mb-6">Latest Blogs</h1>
-     <div >
+    <div className="max-w-6xl mx-auto p-6 space-y-6">
+      <h1 className="text-3xl font-bold mb-6">Most Popular</h1>
+     <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 justify-center  gap-y-10 ">
          {blogs.map((blog) => (
         <div
           key={blog._id}
-          className="border flex justify-between px-4 my-5 rounded-lg p-4 hover:shadow-lg transition"
+          className="border-l-2 px-3 py-5 rounded-lg   hover:shadow-lg transition"
         >
-          <div>
-            <Link  key={blog._id} to={`/blogs/${blog.slug}`} ><h2 className="text-xl font-semibold">{blog.title} <span className="font-light">by</span> {blog.authorName}</h2></Link>
-          <p className="text-gray-600 mb-2">
+      <img
+            src={blog.coverImageUrl}
+            alt={blog.title}
+            className="w-full h-[150px] rounded-lg mb-4"
+          />
+           <p className="text-gray-600 mb-2">
             {new Date(blog.publishedAt).toLocaleDateString()}
           </p>
-          <p className="text-gray-800 h-[60px]">{blog.excerpt}</p>
-          <div className="mt-2 flex flex-wrap gap-2">
+          <p className="text-sm mb-3">Post By <span className="font-bold">{blog.authorName}</span> </p>
+            <Link  key={blog._id} to={`/blogs/${blog.slug}`} ><h2 className="text-xl hover:underline hover:underline-offset-4  text-black font-bold">{blog.title} </h2></Link>
+         
+          {/* <p className="text-gray-800 h-[60px]">{blog.excerpt}</p> */}
+          {/* <div className="mt-2 flex flex-wrap gap-2">
             {blog.tags.map((tag, i) => (
               <span key={i} className="bg-gray-200 px-2 py-1 rounded">
                 {tag}
               </span>
             ))}
+          </div> */}
           </div>
-          </div>
-          <img
-            src={blog.coverImageUrl}
-            alt={blog.title}
-            className="w-[280px] rounded mb-4"
-          />
           
-        </div>
+          
+       
       ))}
      </div>
     </div>
